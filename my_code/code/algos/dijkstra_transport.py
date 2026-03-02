294300d8b66fd124f42c531f4485b04668f8cc70
stanislav.ivanov <stanislav.ivanov@skbkontur.ru>
2026-03-02 14:35:20 +0500

    взвешенный граф

--- my_code/code/algos/dijkstra_transport.py
+++ my_code/code/algos/dijkstra_transport.py
@@ -12,3 +12,42 @@ class DijkstraWithTransport(nk.distance.Dijkstra):
         self.start_position = source
         self.transportRoutes = transportRoutes
         self.speed = speed
+
+
+
+class HybridDijkstraWithMultiplier(nk.distance.Dijkstra):
+    def __init__(self, G, source, multiplier=1.0, **kwargs):
+        super().__init__(G, source, **kwargs)
+        self.multiplier = multiplier
+
+        # СОЗДАЕМ НОВЫЙ ГРАФ с измененными весами
+        # Это самый надежный способ использовать C++ код
+        self.modified_G = self.create_modified_graph(G, multiplier)
+
+    def create_modified_graph(self, G, multiplier):
+        """Создает копию графа с умноженными весами"""
+        n = G.upperNodeIdBound()
+        new_G = nk.Graph(n, weighted=True, directed=G.isDirected())
+
+        # Копируем все ребра с новыми весами
+        for u in range(n):
+            for (v, w) in G.iterNeighbors(u):
+                if u < v or G.isDirected():  # избегаем дублирования
+                    new_G.addEdge(u, v, w * multiplier)
+
+        return new_G
+
+    def run(self):
+        # Заменяем граф на модифицированный и запускаем родительский run
+        original_G = self._G
+        self._G = self.modified_G
+
+        # Вызываем родительский run (весь на C++!)
+        result = super().run()
+
+        # Возвращаем исходный граф (чтобы не сломать другие алгоритмы)
+        self._G = original_G
+
+        return result
+
+    # Можно даже не переопределять explore!
