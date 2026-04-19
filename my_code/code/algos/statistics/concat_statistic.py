a039ef59b2dd86d17778839f2ed90f375fe81cad
stanislav.ivanov <stanislav.ivanov@skbkontur.ru>
2026-04-19 16:49:04 +0500

    сбор статистики переделан

--- my_code/code/algos/statistics/concat_statistic.py
+++ my_code/code/algos/statistics/concat_statistic.py
@@ -1,4 +1,6 @@
-﻿from my_code.code.algos.statistics.statistic_classes import Statistic
+﻿import heapq
+
+from my_code.code.algos.statistics.statistic_classes import Statistic
 
 
 def concat_statistic(statistic: list[Statistic]) -> Statistic:
@@ -7,6 +9,94 @@ def concat_statistic(statistic: list[Statistic]) -> Statistic:
 
     start = statistic[0]
 
-    for stat in statistic[1:]:
-        start += stat
+    for other in statistic[1:]:
+        start = concat_pair_statistic(start, other)
     return start
+
+def concat_pair_statistic(self : Statistic, other : Statistic) -> Statistic:
+    self.mean_statistic.n += other.mean_statistic.n
+    self.mean_statistic.s += other.mean_statistic.s
+
+    #
+    per = self.statistic_percentel
+    per_o = other.statistic_percentel
+    if per.s != per_o.s or per.e != per_o.e or per.step != per_o.step:
+        raise Exception('не могу смёржить статистику по общим перцентелям')
+
+    per.n += per_o.n
+    for i in range(len(per.array)):
+        per.array[i] += per_o.array[i]
+
+    #
+    for key in self.statistic_means_for_nodes.d_means_sum:
+        if key not in other.statistic_means_for_nodes.d_means_sum:
+            raise Exception('Не могу смёржить статистики по среднему для вершин')
+
+        self.statistic_means_for_nodes.d_means_sum[key] += other.statistic_means_for_nodes.d_means_sum[key]
+        self.statistic_means_for_nodes.d_means_count[key] += other.statistic_means_for_nodes.d_means_count[key]
+
+    #
+    all_per = self.statistic_percentel
+    all_per_o = other.statistic_percentel
+    if all_per.s != all_per_o.s or all_per.e != all_per_o.e or all_per.step != all_per_o.step:
+        raise Exception('Не могу смёржить статистику по перцентелям для вершин')
+
+    for key in self.statistic_percentel_for_nodes.d_arrays:
+        if key not in other.statistic_percentel_for_nodes.d_arrays:
+            raise Exception('Не могу смёржить статистику по перцентелям для вершин')
+
+        for i in range(len(self.statistic_percentel_for_nodes.d_arrays[key])):
+            self.statistic_percentel_for_nodes.d_arrays[key][i] += other.statistic_percentel_for_nodes.d_arrays[key][i]
+        self.statistic_percentel_for_nodes.d_n[key] += other.statistic_percentel_for_nodes.d_n[key]
+
+    #
+    if self.statistic_max_pairs.amount_elements != other.statistic_max_pairs.amount_elements:
+        raise Exception('Нем можем склеить статистику по худшим дистанциям')
+
+    while len(other.statistic_max_pairs.heap) > 0:
+        pair_from_other = heapq.heappop(other.statistic_max_pairs.heap)
+        heapq.heappush(self.statistic_max_pairs.heap, pair_from_other)
+        if len(self.statistic_max_pairs.heap) > self.statistic_max_pairs.amount_elements:
+            heapq.heappop(self.statistic_max_pairs.heap)
+
+    return self
+
+if __name__ == "__main__":
+    d = {
+        "mean" : "True",
+
+        "percentel" : "True",
+        "percentel_s" : "0",
+        "percentel_e": f"{100}",
+        "percentel_step": "1",
+
+        "mean_for_nodes" : "True",
+
+        "percentel_for_nodes" : "True",
+        "percentel_for_nodes_k" : "True",
+        "max_pairs" : "True",
+        "max_pairs_amount" : "4",
+
+        "func_log" : "Tre",
+    }
+    s = Statistic(d)
+
+    s.process({
+        (1, 1): 10, (2, 2) : 20, (3, 3): 30
+    })
+
+    s_2 = Statistic(d)
+    s_2.process({
+        (1, 1): 10, (2, 2): 20, (3, 3): 30
+    })
+
+    s_2.process({
+        (1, 1): 5, (2, 2): 10, (1, 3): 25
+    })
+    s = concat_statistic([s, s_2])
+    print(s.mean_statistic.get_mean())
+    print(s.statistic_percentel.get_percentel(0.5))
+    print(s.statistic_means_for_nodes.get_mean(3))
+    print(s.statistic_percentel_for_nodes.get_percentel(3, 0.5))
+    print(s.statistic_max_pairs.get_pairs())
+
--- my_code/code/algos/statistics/statistic_classes.py
+++ my_code/code/algos/statistics/statistic_classes.py
@@ -1,8 +1,30 @@
-﻿from typing import Callable, Any
-
+﻿from multiprocessing import heap
+from typing import Callable, Any
+import heapq
 import datasketches
 from datasketches import kll_floats_sketch
 from marshmallow.fields import Boolean
+from math import log
+
+def get_func(d: dict[str, str]) -> Callable[[int, int, float], float]:
+    d_func = {
+        "func_log" : lambda _0, _1, x : log(x)
+    }
+    for k, v in d_func.items():
+        if k in d and d[k].upper() == "TRUE":
+            return v
+    return lambda _0, _1, x: x
+
+def get_int(string: str, d: dict[str, str]):
+    try:
+        if string not in d:
+            raise Exception(f'Нет в словаре необходимого ключа = {string}')
+        num = int(d[string])
+        if num < 0:
+            raise Exception(f'{string} = {d[string]} должно быть больше 0 либо равно')
+        return num
+    except ValueError:
+        raise Exception(f'{string} = {d[string]} не является int')
 
 class StatisticAbstract:
     def process(self, stat : dict[tuple[int, int], float]) -> None:
@@ -12,7 +34,7 @@ class StatisticMean(StatisticAbstract):
     s : float
     n : int
 
-    def __init__(self):
+    def __init__(self, d : dict[str, str]):
         self.s = 0
         self.n = 0
 
@@ -26,27 +48,76 @@ class StatisticMean(StatisticAbstract):
 
 
 class StatisticPercentel(StatisticAbstract):
-    sketch: kll_floats_sketch
+    s : int
+    e : int
+    step : int
+    array : list[int]
+    n : int
 
-    def __init__(self, k):
-        self.sketch = datasketches.kll_floats_sketch(k)
+    def __init__(self, d : dict[str, str]):
+        self.s = get_int("percentel_s", d)
+        self.e = get_int("percentel_e", d)
+        self.step = get_int("percentel_step", d)
+        end_range = self.e + self.step if (self.e - self.s) % self.step == 0 else self.e + (2 * self.step)
+        self.array = [0 for i in range(self.s - self.step, end_range, self.step)]
+        self.n = 0
 
-    def __getstate__(self):
-        return {'sketch_bytes': self.sketch.serialize()}
 
-    def __setstate__(self, state):
-        self.sketch = kll_floats_sketch.deserialize(state['sketch_bytes'])
+
+    def get_index_for_value(self, v) -> int:
+        pos = int(((v - self.s) // self.step) + 1)
+        if pos <= 0:
+            return 0
+        if len(self.array) - 1 <= pos:
+            return len(self.array) - 1
+        return pos
+
 
     def process(self, stat: dict[tuple[int, int], float]) -> None:
         for key, value in stat.items():
-            self.sketch.update(value)
+            pos = self.get_index_for_value(value)
+            self.array[pos] += 1
+            self.n += 1
+
+    def index_pos(self, rank):
+        return int((self.n * rank + 1) // 1)
 
     def get_percentel(self, rank) -> float:
-        return self.sketch.get_quantile(rank)
+        target = self.index_pos(rank)
+
+        mid_st = (float(self.step) / 2)
+        if self.array[0] > target:
+            return -2.0
+        if self.array[0] + self.array[1] > target:
+            return self.s + mid_st
+
+        c = self.array[0] + self.array[1]
+        i_last = 1 if self.array[1] > 0 else -1
+        c_last = 0
+
+        for i in range(2, len(self.array) - 1):
+            if self.array[i] == 0:
+                continue
+            c += self.array[i]
+            if c >= target:
+                curr_v = float(self.s + (i - 1) * self.step + mid_st)
+                if i_last == -1 or c > target:
+                    return curr_v
+                last_v = float(self.s + (i_last - 1) * self.step + mid_st)
+
+                dist_last = float(target - c_last)
+                dist_cur = float(c - target)
+                return last_v + (curr_v - last_v) * (dist_last / (dist_cur + dist_last))
+            i_last = i
+            c_last = c
+        return -1.0
+
+    def get_array(self):
+        return self.array.copy()
 
 class StatisticMeansForNodes(StatisticAbstract):
     d_means_sum: dict[int, float]
-    d_means_count: dict[int, float]
+    d_means_count: dict[int, int]
 
     def __init__(self):
         self.d_means_sum = {}
@@ -59,52 +130,100 @@ class StatisticMeansForNodes(StatisticAbstract):
                 self.d_means_sum[end] = 0
                 self.d_means_count[end] = 0
             self.d_means_sum[end] += value
-            self.d_means_count[end] += value
+            self.d_means_count[end] += 1
 
     def get_mean(self, node_id) -> float:
         return self.d_means_sum[node_id] / self.d_means_count[node_id]
 
 
 class StatisticPercentelForNodes(StatisticAbstract):
-    d_kll_floats_sketch: dict[int, kll_floats_sketch]
-    k : int
+    d_arrays: dict[int, list[int]]
+    d_n: dict[int, int]
 
-    def __init__(self, k):
-        self.d_kll_floats_sketch = {}
-        self.k = k
 
-    def process(self, stat: dict[int, list[float]]) -> None:
+    def __init__(self, d : dict[str, str]):
+        self.s = get_int("percentel_s", d)
+        self.e = get_int("percentel_e", d)
+        self.step = get_int("percentel_step", d)
+        self.end_range = self.e + self.step if (self.e - self.s) % self.step == 0 else self.e + (2 * self.step)
+        self.len = (self.end_range - (self.s - self.step)) // self.step
+        self.d_arrays = {}
+        self.d_n = {}
+
+
+    def get_index_for_value(self, v) -> int:
+        pos = int(((v - self.s) // self.step) + 1)
+        if pos <= 0:
+            return 0
+        if self.len - 1 <= pos:
+            return self.len - 1
+        return pos
+
+    def process(self, stat: dict[tuple[int, int], float]) -> None:
         for key, value in stat.items():
             start, end = key
-            if end not in self.d_kll_floats_sketch:
-                self.d_kll_floats_sketch[end] = datasketches.kll_floats_sketch(self.k)
-            self.d_kll_floats_sketch[end].update(value)
+            if end not in self.d_arrays:
+                self.d_arrays[end] = [0 for i in range(self.s - self.step, self.end_range, self.step)]
+                self.d_n[end] = 0
+            pos = self.get_index_for_value(value)
+            self.d_arrays[end][pos] += 1
+            self.d_n[end] += 1
+
+    def index_pos(self, node_id, rank):
+        return int((
+                           (self.d_n[node_id] * rank) + 1
+                   )// 1)
 
     def get_percentel(self, node_id, rank) -> float:
-        return self.d_kll_floats_sketch[node_id].get_quantile(rank)
+        target = self.index_pos(node_id, rank)
+        array = self.d_arrays[node_id]
 
-    def __getstate__(self):
-        """Сериализация для передачи между процессами"""
-        # Сериализуем каждый sketch в байты
-        serialized_sketches = {}
-        for node_id, sketch in self.d_kll_floats_sketch.items():
-            serialized_sketches[node_id] = sketch.serialize()
+        mid_st = (float(self.step) / 2)
+        if array[0] > target:
+            return -2.0
+        if array[0] + array[1] > target:
+            return self.s + mid_st
 
-        # Возвращаем состояние в сериализуемом виде
-        state = {
-            'k': self.k,
-            'serialized_sketches': serialized_sketches
-        }
-        return state
+        c = array[0] + array[1]
+        i_last = 1 if array[1] > 0 else -1
+        c_last = 0
 
-    def __setstate__(self, state):
-        """Десериализация после получения в другом процессе"""
-        self.k = state['k']
-        self.d_kll_floats_sketch = {}
+        for i in range(2, len(array) - 1):
+            if array[i] == 0:
+                continue
+            c += array[i]
+            if c >= target:
+                curr_v = float(self.s + (i - 1) * self.step + mid_st)
+                if i_last == -1 or c > target:
+                    return curr_v
+                last_v = float(self.s + (i_last - 1) * self.step + mid_st)
 
-        # Восстанавливаем каждый sketch из байтов
-        for node_id, sketch_bytes in state['serialized_sketches'].items():
-            self.d_kll_floats_sketch[node_id] = datasketches.kll_floats_sketch.deserialize(sketch_bytes)
+                dist_last = float(target - c_last)
+                dist_cur = float(c - target)
+                return last_v + (curr_v - last_v) * (dist_last / (dist_cur + dist_last))
+            i_last = i
+            c_last = c
+        return -1.0
+
+
+class StatisticMaxPairs(StatisticAbstract):
+    heap : list[tuple[float, int, int]]
+    amount_elements : int
+
+    def __init__(self, d : dict[str, str]):
+        self.heap = []
+        self.amount_elements = get_int("max_pairs_amount", d)
+
+    def process(self, stat: dict[tuple[int, int], float]) -> None:
+        for key, value in stat.items():
+            start, end = key
+            heapq.heappush(self.heap, (value, start, end))
+            if len(self.heap) > self.amount_elements:
+                heapq.heappop(self.heap)
+
+    def get_pairs(self) -> list[tuple[float, int, int]]:
+        return sorted(self.heap, reverse=True)
+
 
 
 class Statistic(StatisticAbstract):
@@ -112,6 +231,7 @@ class Statistic(StatisticAbstract):
     statistic_percentel : StatisticPercentel
     statistic_means_for_nodes : StatisticMeansForNodes
     statistic_percentel_for_nodes : StatisticPercentelForNodes
+    statistic_max_pairs : StatisticMaxPairs
 
     __process_statistic : Callable[
         [
@@ -120,101 +240,45 @@ class Statistic(StatisticAbstract):
         None
     ]
 
-    def __init__(self, d_arguments: dict[str, Any], func : Callable[[float], float] = lambda x : x) -> None:
-        self.__func = func
+    def __init__(self, d_arguments: dict[str, Any]) -> None:
+        self.__func = get_func(d_arguments)
         self.parse_arguments(d_arguments)
         pass
 
 
-
     def process(self, stat: dict[tuple[int, int], float]) -> None:
         for key, value in stat.items():
-            stat[key] = self.__func(value)
+            s, e = key
+            stat[key] = self.__func(s, e, value)
         self.__process_statistic(stat)
 
-    def get_k(self, k: str):
-        try:
-            k_num = int(k)
-            if k_num <= 0:
-                raise ValueError
-            return k_num
-        except ValueError:
-            raise Exception(f'k = {k} имеет неправильное значение')
 
-
-    def parse_arguments(self, d : dict[str, Any]):
+    def parse_arguments(self, d : dict[str, str]):
         func_for_process = []
-        if "mean" in d and d["mean"]:
-            self.mean_statistic = StatisticMean()
+        if "mean" in d and d["mean"].upper() == "TRUE":
+            self.mean_statistic = StatisticMean(d)
             func_for_process.append(self.mean_statistic.process)
 
-        if "percentel" in d and d["percentel"]:
-            k = self.get_k(d["percentel_k"]) if "percentel_k" in d else 52_000
-            self.statistic_percentel = StatisticPercentel(k)
+        if "percentel" in d and d["percentel"].upper() == "TRUE":
+            self.statistic_percentel = StatisticPercentel(d)
             func_for_process.append(self.statistic_percentel.process)
 
-        if "mean_for_nodes" in d and d["mean_for_nodes"]:
+        if "mean_for_nodes" in d and d["mean_for_nodes"].upper() == "TRUE":
             self.statistic_means_for_nodes = StatisticMeansForNodes()
             func_for_process.append(self.statistic_means_for_nodes.process)
 
-        if "percentel_for_nodes" in d and d["percentel_for_nodes"]:
-            k = self.get_k(d["percentel_for_nodes_k"]) if "percentel_for_nodes_k" in d else 200
-            self.statistic_percentel_for_nodes = StatisticPercentelForNodes(k)
+        if "percentel_for_nodes" in d and d["percentel_for_nodes"].upper() == "TRUE":
+            self.statistic_percentel_for_nodes = StatisticPercentelForNodes(d)
             func_for_process.append(self.statistic_percentel_for_nodes.process)
-        self.func_for_process = func_for_process
 
+        if "max_pairs" in d and d["max_pairs"].upper() == "TRUE":
+            self.statistic_max_pairs = StatisticMaxPairs(d)
+            func_for_process.append(self.statistic_max_pairs.process)
+
+        self.func_for_process = func_for_process
 
         self.__process_statistic = self.process_stat
 
     def process_stat(self, stat: dict[tuple[int, int], float]) -> None:
         for func in self.func_for_process:
             func(stat)
-
-    def __add__(self, other_stat):
-        self.mean_statistic.n += other_stat.mean_statistic.n
-        self.mean_statistic.s += other_stat.mean_statistic.s
-        self.statistic_percentel.sketch.merge(other_stat.statistic_percentel.sketch)
-
-        for key in self.statistic_means_for_nodes.d_means_sum:
-            if key not in other_stat.statistic_means_for_nodes.d_means_sum:
-                raise Exception('не могу смёржить статистики')
-
-            self.statistic_means_for_nodes.d_means_sum[key] += other_stat.statistic_means_for_nodes.d_means_sum[key]
-
-        for key in self.statistic_means_for_nodes.d_means_count:
-            if key not in other_stat.statistic_means_for_nodes.d_means_count:
-                raise Exception('не могу смёржить статистики')
-            self.statistic_means_for_nodes.d_means_count[key] += other_stat.statistic_means_for_nodes.d_means_count[key]
-        return self
-
-
-if __name__ == "__main__":
-    d = {
-        "mean" : True,
-
-        "percentel" : True,
-        "percentel_k" : 100,
-
-        "mean_for_nodes" : True,
-
-        "percentel_for_nodes" : True,
-        "percentel_for_nodes_k" : 1000
-    }
-    s = Statistic(d)
-
-    s.process({
-        (1, 1): 10, (2, 2) : 20, (3, 3): 30
-    })
-
-    s.process({
-        (1, 1): 10, (2, 2): 20, (3, 3): 30
-    })
-
-    s.process({
-        (1, 1): 5, (2, 2): 10, (3, 3): 15
-    })
-
-    print(s.mean_statistic.get_mean())
-    print(s.statistic_percentel.get_percentel(0.5))
-    print(s.statistic_means_for_nodes.get_mean(2))
-    print(s.statistic_percentel_for_nodes.get_percentel(2, 0.5))
